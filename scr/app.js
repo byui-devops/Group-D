7c0b1994ee5631705b0f8483dff7f48ee674025d
Christian Taylor <72665090+AnhMeo@users.noreply.github.com>
2025-07-24 09:50:21 +0700

    Create app.js

---
+++ scr/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const routes = require('./routes');
+const app = express();
+app.use(express.json());
+routes(app);
+app.listen(3000, () => console.log('Server running on port 3000'));
