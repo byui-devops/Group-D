bfc49a80fef22433d71a4be9ff6f3c3246d73ca4
Christian Taylor <72665090+AnhMeo@users.noreply.github.com>
2025-07-24 10:25:48 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@
    variable "ami_id" {
      description = "AMI ID for the EC2 instance"
      type        = string
-     default     = "ami-0017ad631c7152120" 
+     default     = "ami-0039a65b96d31e173" 
    }
 
    variable "key_pair_name" {
