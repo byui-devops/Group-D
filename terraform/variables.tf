1b05727af7b7b41035e3dd5b3c34dca27c7c86be
Christian Taylor <72665090+AnhMeo@users.noreply.github.com>
2025-07-24 08:35:29 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1" # Matches Account ID 164675867921
+  default     = "us-east-1" 
 }
 
 variable "ami_id" {
