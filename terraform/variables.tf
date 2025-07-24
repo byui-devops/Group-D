0e634e2d163cfcd40a9033ecf082d43cabd91707
Christian Taylor <72665090+AnhMeo@users.noreply.github.com>
2025-07-24 11:05:25 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,8 +15,3 @@
      type        = string
      default     = "vockey" 
    }
-
-output "instance_public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.web.public_ip
-}
