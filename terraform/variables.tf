70c17bb3050c516cf93872d7c808ef12647559ab
Christian Taylor <72665090+AnhMeo@users.noreply.github.com>
2025-07-24 08:35:15 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,11 +7,35 @@ variable "region" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c55b159cbfafe1f0" #
+  default     = "ami-0010bb3c78c898b92" 
 }
 
 variable "key_pair_name" {
   description = "SSH key pair name"
   type        = string
-  default     = "your-key-name" 
+  default     = "MIIEpAIBAAKCAQEAzeoMBNVugqDmYcpZ7FuSbvBQebAVrDYBsbhGTrn0alKp0Jo9
+bp1P88ZCi+7/lvXrRlhwvYA2k6mZgi0IKlacy2mQvZRU95yDC2JPAyCoL4jN1Jyz
+43dH0qoHcVZ76Lp7hoWpyBUebrVP4wANrIL7KY5izzUPMLwPTNxNoA8WavdyWZ9T
+G1agKfy3bNxBQIZ2fIKZORZW/sW88UfgezCfFkh6GKzDm0OqGkS2CrZmtFnMPENP
+358BoGmZRKxUP0sXkEOH5j9b+Gem5M4Ch7mSjMj9VsJu4nUM9r9UOJaKmS9U2TsQ
+HQbLBmgvL9UrhCCddTLas/+BAVZeCHGdzm2bwwIDAQABAoIBAEy+Ara2eNZUcyhV
+CwiM5FlWVRfVKW6iPiVnIUTsBwytPD7TjTuxg3++D0Up0LYJdyMjiPkHVM/ZsK2U
+bOuilhM1h80wAC68dRaEECCBRk1Y9iVcNar75Je96MIz0kBdZ/Yt0KYSQOJPzrMy
+bOYzdrn5KBhDlSK7pBQoMwIJPXiljx667UbRrkizmmAB7LxwpFGLtkvrf2AtEYDr
+UC3Hje1AOOWsl/bpbYvowOmBhTKzeFtCusCxRprDbIMcyoq5gQe2uM8NXesZ7/XK
+u8GGKOxRhwiBotKO93lT6z1xPz4Quz6O/Lp1g5Mq9rLJAwCx5s17yIYljaYRBH2L
+sBviHnECgYEA6TrwhOBpG+tDILu3wtJgMALzCWR7Le9aH/2RGtXVn2FO7OojtR9L
+46zztyMvh1S0kHCvaEMrRX3RLPpjKZNAoGgXvwZy4qZOd9eYDu7GNNobBJyV/Lf8
+Ol1Gd8qcTJv5DsWrTPG5NMyAX4HnFZaA/kLN2KZiQB6FpECx4FltXPsCgYEA4gRo
+BDMpai9hwasc4HdhHldENGMWfOwzB3igUhH76ECaG/9zBMiyFOqKfyiRW1IDV6rN
+dMQEjyO3ca6rlmbIEgs25kwDjV22gQGBsUVFTWJpLkDxTu/f4fOAFkgCjpMiKEaU
+ujaxhf6OW1TtiEpJoUdPAhiI5BHWPk6ziILdcdkCgYEA44zqv2nHg0LNErGzfzQM
+PquR/Zb4i3+cWdvmZ907eC3TeFiUkBFB1JeYjRSaOZpONSKMMNhf7oPrwstkVlrh
+0d7dVDXxfuqtYkWJiQAzh7tIYv53Y8MKJtN8fkHCrwf6PgxjrkWk4juoAAbYs/pM
+TkZVWcwzzcU5tGCea3o0tmMCgYABnExtczvhUNMegSykgNVFVRPcV7RqUXiPnhFu
+ZEhqQEOY3XohV5jFQ2KXQ1+9ydjSQBpUsazipDuDaN46JSNZ1ZCflX0U/cz2IF+f
+NsImqgPb24ScrmLnBkgsYHj8MRo2NUKMDSW4sbar+e/zX6wkpRBwEGKxVVs0eHUZ
+DO2tCQKBgQC0nKZkNAI2CFglWrjsh14ZE/9OnhTP+OOq2CJkDgcLgtUkwaDpHBzr
+hV5EQjzHS5THtwUCjavZcJKFegG1l0Q2zpS4VlfAL7nTB1Np7aWQtaya1oIIodOt
+zW+uOesR9dhW1JYAWw/MX2nmZ/oQv3gQQoAUWGh1Mu/P8htLTMT+uQ==" 
 }
